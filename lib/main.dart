c490af5801fcaf9fe706a74275d38b10a3953db3
ZERONE <zerone012@gmail.com>
2023-09-14 20:39:11 +0900

    UI 공부 중. PhotoView / PhotoViewGallery

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:study_flutter_13_totally03_lec_photo/photo_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,13 +29,32 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<String> imagePaths = [
+    'https://cdn.pixabay.com/photo/2023/04/30/09/43/flower-7960192_1280.jpg',
+    'https://cdn.pixabay.com/photo/2023/04/30/22/01/ocean-7961695_1280.jpg',
+    'https://cdn.pixabay.com/photo/2023/05/21/07/59/iceberg-8008071_1280.jpg',
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('')),
-      body: const Center(
-        child: Column(
-          children: [],
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(title: const Text('PhotoView - Main')),
+        body: ListView.builder(
+          itemCount: imagePaths.length,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => PhotoViewPage(
+                      imagePaths: imagePaths, currentIndex: index),
+                ));
+              },
+              child: Image(
+                image: NetworkImage(imagePaths[index]),
+              ),
+            );
+          },
         ),
       ),
     );
